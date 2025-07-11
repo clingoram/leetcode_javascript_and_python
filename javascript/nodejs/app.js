80962a220229aa00e0190a6b4b5f0efbca360e90
Mavis <llingchen.27@gmail.com>
2025-07-11 13:12:46 +0800

    learn node.js

--- javascript/nodejs/app.js
+++ javascript/nodejs/app.js
@@ -5,7 +5,6 @@ const port = 3000;
 const server = createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  url
   res.end('Hello World');
 });
 server.listen(port, hostname, () => {
