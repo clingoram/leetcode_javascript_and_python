65f01f4e5767fa1366c3fd66f19f7d81e9149b23
Mavis <llingchen.27@gmail.com>
2026-05-12 14:32:56 +0800

    practice to solve no.2553

--- javascript/index.js
+++ javascript/index.js
@@ -1404,3 +1404,35 @@ let s = "010";
 // Output: "001"
 // Explanation: Because there is just one '1', it must be in the last position. So the answer is "001".
 // console.log(maximumOddBinaryNumber(s));
+
+/**
+ * 2553. Separate the Digits in an Array
+ * 
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+    /*
+    將參數元素依照位數拆開之後依照原本的順序合併
+    */
+    let ans = [];
+    for(let i = 0;i < nums.length;++i) {
+      while(nums[i] > 0){
+          ans.push(nums[i] % 10);
+          nums[i]/=10;
+        }
+    }
+    console.log(ans);
+
+};
+let nums = [13,25,83,77];
+/*
+Output: [1,3,2,5,8,3,7,7]
+Explanation: 
+- The separation of 13 is [1,3].
+- The separation of 25 is [2,5].
+- The separation of 83 is [8,3].
+- The separation of 77 is [7,7].
+answer = [1,3,2,5,8,3,7,7]. Note that answer contains the separations in the same order.
+*/
+console.log(separateDigits(nums));
